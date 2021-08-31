dd11e26bf7566698f3fe71ed5cd876b63530deb9
whitehat-G6 <88480121+WHJR-G6@users.noreply.github.com>
2021-08-31 14:26:21 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 var ship, ship_sailing, edges;
 var seaImage;
 var sea,ship;
+
 function preload(){
- 
+  ship_sailing= loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png")
   
 }
 
@@ -10,6 +11,7 @@ function setup(){
   createCanvas(900,900);  
   ship = createSprite(400,300,100,100);
   edges = createEdgeSprites();
+  ship.addAnimation("ship",ship_sailing)
 }
 
 function draw(){
